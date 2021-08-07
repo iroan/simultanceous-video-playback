0f3e19fdb4e8584576ed2816cdc52d4f6c0c28b0
iroan <2654189525@qq.com>
2021-08-08 01:32:48 +0800

    add func

--- operation.sh
+++ operation.sh
@@ -7,4 +7,14 @@ function rds() {
     mysql --login-path=root --database svp --table <mysql.sql >"${filename}"
 }
 
+function redis() {
+    set +x
+    redis-cli -h amd -p 12052 -a 9e15-a7de0e9792a8
+    set -x
+}
+
+function curlPostNewSession() {
+    curl -X POST http://192.168.50.174:18605/status -d @newSession.json
+}
+
 $1
